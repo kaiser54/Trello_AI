5a0139e4803dc51eb3afed8f82023d3edf5a68ab
kaiser54 <agboolatemitope070@gmail.com>
2023-11-01 23:56:17 +0100

    get trello columns with data, sort data

--- lib/getTodosGroupedByColumn.ts
+++ lib/getTodosGroupedByColumn.ts
@@ -30,6 +30,27 @@ export const getTodosGroupedByColumn = async () => {
     // if columns doesnt have the inprogress, todo, and done, add them with an empty todos array
     const columnTypes: TypedColumn[] = ['TODO', 'IN_PROGRESS', 'DONE'];
     for (const columnType of columnTypes) {
-        
-    }
-}
+        if(!columns.get(columnType)) {
+            columns.set(columnType, {
+                id: columnType,
+                todos: []
+            })
+        }
+    };
+
+    // sort columns by the columnTypes
+
+    const sortedColumns  = new Map (
+        Array.from(columns.entries()).sort((a, b) => (
+            columnTypes.indexOf(a[0]) - columnTypes.indexOf(b[0]) 
+        ))
+    );
+
+    console.log(sortedColumns);
+
+    const board: Board = {
+        columns: sortedColumns,
+    };
+
+    return board
+};
